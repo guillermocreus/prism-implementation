bea1858a84d68c7793063318dd85ab0054abfa1c
Guillermo Creus <guillermocreus98@gmail.com>
2022-04-12 01:47:18 +0200

    delete old file

--- src/prism.py
+++
@@ -1,8 +0,0 @@
-from sklearn.base import BaseEstimator
-
-class ResampledEnsemble(BaseEstimator):
-
-    def __init__(self, n_rules=20):
-        
-        self._estimator_type = "classifier"
-        self.n_rules = n_rules
